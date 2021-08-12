edb7082ee7e4ff9e229c4b9dc7f8252647cdddcb
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-08-12 12:16:15 +0200

    modifing and styling ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item!!</h2>;
+  return (
+    <div className="expense-item">
+      <div>July 21st 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">€300.45</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

45ae0eb844af2c042813d1a73be28626666c528f
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-08-13 11:23:58 +0200

    Expenses component created, Card component created. props children and styling

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import "./Card.css";
+
+function Card(props) {
+  //Card is a custom component, no default values-->
+  //card have to accept all the values that comes as props
+  const classes = "card " + props.className;
+
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;

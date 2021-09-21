184061715579a19cb6a72a014e7bd19daa12aa50
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-09-21 21:24:34 +0200

    listener test

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,16 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
+// we test a button element to show how to trigg an event
+// onClick could just have an anonimous function, but it's better to create a function (clickHandler)
+// and invoke it from onClick: no too much logic in the JSX!
+// No parentesis () efter clickHandler (22). The function doesn't have to start right away, but just "on click"!!!
+
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +19,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">€{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };

510eac63894733eec8664031ccb20d7eb4b9238c
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-09-21 21:54:40 +0200

    Testing state with the hook useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,21 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-// we test a button element to show how to trigg an event
-// onClick could just have an anonimous function, but it's better to create a function (clickHandler)
-// and invoke it from onClick: no too much logic in the JSX!
-// No parentesis () efter clickHandler (22). The function doesn't have to start right away, but just "on click"!!!
-
 const ExpenseItem = (props) => {
+  //We are using a hooks, useState.
+  //It needs a default value(argument). This value is read just the first time we mount the component
+  //If we mount the component again, the default becomes the last state
+  // we destructure the name (title) and the function to change it(setTitle)
+
+  const [title, setTitle] = useState(props.title);
+
+  // we use setTitle in the function and title in the JSX(26).
+  // In this way we are modifying the DOM "on Click"
   const clickHandler = () => {
+    setTitle("Updated!!");
     console.log("Clicked!!!");
   };
 
@@ -16,7 +23,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">€{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>

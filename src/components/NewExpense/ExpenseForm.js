457ce496684fb7480c0eb6cefc77162bfab20d03
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2021-09-29 00:02:57 +0200

    added two-way binding, from input to state, from state to input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -60,21 +60,36 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
+    // we clear the input
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+
     console.log(expenseData);
   };
 
   // onSubmit listener would reload the page as default
+  // two-way binding: the input into the state, the state into the input
+  // we can change the state as we want, the result will appear in the input box¨
+  // in this case we will use the twb to clear the input
+  // We do it just setting the state as value (84,92,102)
+
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={enteredAmount}
             onChange={amountChangeHandler}
             min="0.01"
             step="0.01"
@@ -84,6 +99,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             onChange={dateChangeHandler}
             min="2021-01-01"
             max="2024-12-31"

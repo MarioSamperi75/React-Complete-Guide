883d96291619c6adca796728140c50ca59b37e2f
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-19 11:47:02 +0100

    rendering a new expense

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React from "react";
-// We can pass props from parent to child
-// How to pass things from child to parent?
-// we will pass now the new expense to App (ExpenseForm=>NewExpense=>App)
-// Steps: create a function in the parent, pass the function as a props ...
-//... invoke the function from the child and pass things as argument!
-// we have to do it 2 times, ExpenseForm/NewExpense  and NewExpense/App
-
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+
+  //  we get the new expense from the child
+  //  we add to the previous array
+  //  and set it as state
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses];
+    });
   };
 
   return (
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,47 +7,16 @@ const ExpenseForm = (props) => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
-  //instead of three states, just one as an object
-  // const [userInput, setUserInput] = useState({
-  //   enteredTitle: "",
-  //   enteredAmount: "",
-  //   enteredDate: "",
-  //});
-
-  // when you depend on the previos state, use this syntax!!!
-  // you will assure that the last updating is really the last one
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-
-    // setUserInput((prevState) => {
-    //   return {
-    //     ...prevState,
-    //     enteredTitle: event.target.value,
-    //   };
-    // });
-    // console.log(userInput.enteredTitle);
-    console.log(enteredTitle);
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enteredAmount: event.target.value };
-    // });
-    // console.log(userInput.enteredAmount);
-    console.log(enteredAmount);
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-    // setUserInput((prevState) => {
-    //   return {
-    //     ...prevState,
-    //     enteredDate: event.target.value,
-    //   };
-    // });
-    //console.log(userInput.enteredDate);
-    console.log(enteredDate);
   };
 
   const submitHandler = (event) => {
@@ -64,16 +33,9 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
-    // we use expenseData as argument in a function
-    // that we get as props, but that triggers in the parent (with an argument coming from the child : expenseData)
+
     props.onSaveExpenseData(expenseData);
   };
-
-  // onSubmit listener would reload the page as default
-  // two-way binding: the input into the state, the state into the input
-  // we can change the state as we want, the result will appear in the input box¨
-  // in this case we will use the twb to clear the input
-  // We do it just setting the state as value (84,92,102)
 
   return (
     <form onSubmit={submitHandler}>

3eed318b33bc23f2aeef2ef65c1469c752f9dd58
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-19 11:04:48 +0100

    Rendering arrays: map to JSX elements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,6 +13,8 @@ const Expenses = (props) => {
     console.log(selectedYear);
   };
 
+  // instead of hardcoding ExpenseItems we transorm (map) the array of expenses
+  // to an array of JSX Elements (ExpensesItem)
   return (
     <div>
       <Card className="expenses">
@@ -20,26 +22,14 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );

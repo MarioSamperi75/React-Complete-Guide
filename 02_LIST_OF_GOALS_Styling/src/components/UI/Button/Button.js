49546597597178f7e18320145e5ce007e40efa3d
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-21 13:38:51 +0100

    CSS Modules: the button component

--- 02_LIST_OF_GOALS_Styling/src/components/UI/Button/Button.js
+++ 02_LIST_OF_GOALS_Styling/src/components/UI/Button/Button.js
@@ -1,53 +1,19 @@
-// import React from "react";
-import styled from "styled-components";
+import React from "react";
 
-// button is like a method that allows us to set a style to a button
-// with a syntax similar to CSS
-// this button method returns a new button component
-// there is methods for all the HTML elements
-// the new element will apply all the props we pass in the same way
+// new way of importing css to use CSS modules
+// you have also to rename the css file to Button.module.css
+// in this way the css file is scoped to the component
+// and we have not to change the CSS file
+// we can continue to have it as a separate file
+import styles from "./Button.module.css";
 
-//Adapting CSS to styled-components : Steps
-// remove the first .button selector and brackets
-// remove .button from sudo-selector and add & instead
-
-// we apply media queries in the usual way
-//check the bottom width: from 100 %  to auto
-
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-
-  @media (min-width: 768px) {
-    width: auto;
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
-
-// import './Button.css';
-
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+// the class becomes an object
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;

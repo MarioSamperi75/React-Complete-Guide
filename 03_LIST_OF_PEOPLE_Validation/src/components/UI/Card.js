89e6cf81b70eb3b2ce34b6d06b1c3447e1e4e9cb
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-21 21:53:42 +0100

    styling addUser and wrapper components(Card)

--- 03_LIST_OF_PEOPLE_Validation/src/components/UI/Card.js
+++ 03_LIST_OF_PEOPLE_Validation/src/components/UI/Card.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 import classes from "./Card.module.css";
+
+// Card is a component that we created
+// it has not default props
+// if we want to use className as Card attribute (in AddUser.js)
+// we need to send it here as props!
 const Card = (props) => {
-  return <div className={classes.card}>{props.children}</div>;
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  );
 };
 
 export default Card;

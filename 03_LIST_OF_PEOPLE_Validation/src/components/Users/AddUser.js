a17d9482228b1f0877319bd91ec79cf5f972c49c
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-24 13:01:03 +0100

    creating ErrorModal and backdrop

--- 03_LIST_OF_PEOPLE_Validation/src/components/Users/AddUser.js
+++ 03_LIST_OF_PEOPLE_Validation/src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 import classes from "./AddUser.module.css";
 
@@ -36,25 +37,31 @@ const AddUser = (props) => {
   // type is actually a props that we create.
   // Button is our Component, it has not default attributes
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
-        />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal
+        title="En error occurred !"
+        message="Something went wrong !"
+      />
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 

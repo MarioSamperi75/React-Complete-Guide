cc761b38e171b9f614273fd395934c210fa83beb
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-24 12:27:14 +0100

    managing a List of Students via State

--- 03_LIST_OF_PEOPLE_Validation/src/App.js
+++ 03_LIST_OF_PEOPLE_Validation/src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersLists) => {
+      return [
+        ...prevUsersLists,
+        { id: Math.random().toString(), name: userName, age: userAge },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }

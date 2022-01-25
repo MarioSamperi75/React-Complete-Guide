2a85bfe2d55d1e38adbdb28e723e44e039159288
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-01-25 23:10:09 +0100

    working with refs - getting the inputs

--- 03_LIST_OF_PEOPLE_Validation/src/components/Users/AddUser.js
+++ 03_LIST_OF_PEOPLE_Validation/src/components/Users/AddUser.js
@@ -1,6 +1,19 @@
-// Wrapper allows to render just one thing
-// without creating and rendering a div
-import React, { useState } from "react";
+// Ref as alternative to the state to get the input.
+// Updating the state for every keystroke is not efficient
+// ref allows to access the current object for every element
+// the current object has a "value" that you can get!!!
+
+// Steps:
+// 1) import the hook useRef
+// 2) assign the hook to a variable
+// 3) use the "ref" props in the element you want to refer to
+// 3) ref will have the hook variale as value
+// 4) get the value from the current object
+
+// we get rid of states, change handlers, onChange and value props
+
+// 1) import the hook useRef
+import React, { useState, useRef } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -10,22 +23,20 @@ import Wrapper from "../Helpers/Wrapper";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
-  // we need false as default
+  // 2) assign the hook to a variable
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
 
   const addUserHandler = (event) => {
     //to prevent the reloading of the page on submit
     event.preventDefault();
+
+    //4) get the value from the current object
+    const enteredUsername = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+
     //basic validation logic
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
@@ -43,8 +54,14 @@ const AddUser = (props) => {
     }
 
     props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
+    // we have not anymore the possibility to reset the state
+    // but we can reset the current.value
+    // in general is not a good practice to manipulate directly the DOM
+    // react should do it for us
+    // but in this case it´s not so much, just the user input
+    // otherwise, we need to use states
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   // on click to div or button, to remove the modal
@@ -66,19 +83,9 @@ const AddUser = (props) => {
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>

88ea99ada33ea8c9807cbc41f8576fe6ba51b2fd
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-01 12:19:55 +0100

    ReactMeals: mealItem - the mealItemForm and the Input UI

---
+++ 05_REACTMEALS_Practice_Project/src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import classes from "./Input.module.css";
+
+//we pass input as an object, and we spread all its properties
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;

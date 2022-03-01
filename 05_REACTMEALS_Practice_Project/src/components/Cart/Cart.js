31fc2b26190950f77353e031a537121ee5327dec
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-01 15:48:47 +0100

    ReactMeals: Created Cart in a Modal, set Modal in a Portal

---
+++ 05_REACTMEALS_Practice_Project/src/components/Cart/Cart.js
@@ -0,0 +1,31 @@
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const cartDummyData = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }];
+
+const Cart = (props) => {
+  // we go through the dummy data inside an unordered list
+  // we have to use a different syntax for the className because of the '-'
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartDummyData.map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>34.96</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;

92ef7794bea21d656714c400be3f4655cbb3d6b4
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-04 21:03:21 +0100

    ReactMeals: showing items in the cart

--- 05_REACTMEALS_Practice_Project/src/components/Cart/Cart.js
+++ 05_REACTMEALS_Practice_Project/src/components/Cart/Cart.js
@@ -1,15 +1,42 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 
-const cartDummyData = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }];
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
-  // we go through the dummy data inside an unordered list
-  // we have to use a different syntax for the className because of the '-'
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `€${cartCtx.totalAmount.toFixed(2)}`;
+  // we added a CartItem component and set in the list
+  // we need also two functoins to edit the quantity from the cart
+
+  const cartItemAddHandler = (item) => {};
+
+  const cartItemRemoveHandler = (id) => {};
+
+  // we delete the dummy array and we add context to show the items in the cart
+  // and we change the hardcoded values with real values in the rendered view
+  // we show also the order button just if there's something in the cart
+  const hasItems = cartCtx.items.length > 0;
+
+  // bind preconfigure the function for future executions and preconfigures the arguments too
+  // to ensure that the functions receive the arguments
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {cartDummyData.map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        </li>
       ))}
     </ul>
   );
@@ -18,13 +45,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>34.96</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );

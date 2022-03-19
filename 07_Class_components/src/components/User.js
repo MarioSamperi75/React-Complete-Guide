83d06946ce010619c1850be81920ea640849c450
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-19 09:36:14 +0100

    Class_components: converting a functional component to a class component

--- 07_Class_components/src/components/User.js
+++ 07_Class_components/src/components/User.js
@@ -1,7 +1,17 @@
-import classes from './User.module.css';
+import { Component } from "react";
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// how to convert a functional component to a class component:
+// total compatible with functional components
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
 
 export default User;

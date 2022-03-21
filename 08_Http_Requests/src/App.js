6affce607792986a3d5813712f2fa81f2cdafe17
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-21 14:40:09 +0100

    Http_request: adding loading message

--- 08_Http_Requests/src/App.js
+++ 08_Http_Requests/src/App.js
@@ -5,34 +5,17 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  //  we use the function fetch, that get a promise
+  //  we use the function fetch, that gets a promise
   //  we get a Json object that we transform to a js object by using json()
   //  and then we update a state containing the array of moovies
   //  transformedMovies: we have also to adapt the json field names
   //  so that they match the names we have in our components
 
-  // promise structure
-  // const fetchMoviesHandler = () => {
-  //   fetch("https://swapi.dev/api/films/")
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       const transformedMovies = data.results.map((movieData) => {
-  //         return {
-  //           id: movieData.episode_id,
-  //           title: movieData.title,
-  //           openingText: movieData.opening_crawl,
-  //           releaseDate: movieData.release_date,
-  //         };
-  //       });
-  //       setMovies(transformedMovies);
-  //     });
-  //};
-
-  //  async structure
+  //  async/await  structure
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -45,6 +28,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -53,7 +37,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Movies not found.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );

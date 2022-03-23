7b370b191c4a8a8f56353bd17ec4289332fc2eee
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-23 10:22:30 +0100

    COUNTER_Custom_Hooks: passing arguments to the custom hook

--- 09_COUNTER_Custom_Hooks/src/hooks/use-counter.js
+++ 09_COUNTER_Custom_Hooks/src/hooks/use-counter.js
@@ -2,18 +2,26 @@
 // that can return something, as usual
 // But it allows also to use states
 // you must just start the name with 'use' an import it where needed
+
+// you can pass arguments as usual in order to customize your hook
+// in this case is just a true or false (default true)
+// to choose between forward and backward counter
 import { useState, useEffect } from "react";
 
-const useCounter = () => {
+const useCounter = (forward = 1) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forward) {
+        setCounter((prevCounter) => prevCounter + forward);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forward]);
 
   return counter;
 };

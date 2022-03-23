152023efaf227dbb80c9c3ab4606ac944a003f86
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-23 10:52:55 +0100

    TASK_MANAGER_Custom_Hooks: starting project

--- 09_COUNTER_Custom_Hooks/src/hooks/use-counter.js
+++ 09_COUNTER_Custom_Hooks/src/hooks/use-counter.js
@@ -5,23 +5,23 @@
 
 // you can pass arguments as usual in order to customize your hook
 // in this case is just a true or false (default true)
-// to choose between forward and backward counter
+// to choose between forwards and backwards counter
 import { useState, useEffect } from "react";
 
-const useCounter = (forward = 1) => {
+const useCounter = (forwards = 1) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (forward) {
-        setCounter((prevCounter) => prevCounter + forward);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + forwards);
       } else {
         setCounter((prevCounter) => prevCounter - 1);
       }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [forward]);
+  }, [forwards]);
 
   return counter;
 };

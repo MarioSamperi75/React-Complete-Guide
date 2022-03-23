ebdb9ad7aeafcf7c9d4532dda0c623fccdda99f4
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-23 09:55:13 +0100

    COUNTER_Custom_Hooks: creating the first custom hook

--- 09_COUNTER_Custom_Hooks/src/hooks/use-counter.js
+++ 09_COUNTER_Custom_Hooks/src/hooks/use-counter.js
@@ -1,2 +1,21 @@
-useCounter = () => {};
+// A custom hook is just a function
+// that can return something, as usual
+// But it allows also to use states
+// you must just start the name with 'use' an import it where needed
+import { useState, useEffect } from "react";
+
+const useCounter = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return counter;
+};
+
 export default useCounter;

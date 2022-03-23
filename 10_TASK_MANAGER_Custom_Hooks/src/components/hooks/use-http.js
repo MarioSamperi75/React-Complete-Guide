aafe21fbcdcff8d56ac845b4a278a2138e4dc9aa
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-23 15:19:01 +0100

    TASK_MANAGER_Custom_Hooks: creating an http hook

---
+++ 10_TASK_MANAGER_Custom_Hooks/src/components/hooks/use-http.js
@@ -0,0 +1,38 @@
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      // fuction that we get as argument
+      // the trasformation of the data is different in get or post requests
+      // we delegate the trasformation function to the component that uses this hook
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+  // this syntax is the same as isLoading: isLoading, ... and so on
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;

352a62c66c2401cc884b8c70e8cc35fbf1be4614
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-24 15:45:47 +0100

    Validation: fetching input using ref

--- 11_Validation/src/components/SimpleInput.js
+++ 11_Validation/src/components/SimpleInput.js
@@ -3,6 +3,8 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
 
+  const nameInputRef = useRef();
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -11,13 +13,21 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>

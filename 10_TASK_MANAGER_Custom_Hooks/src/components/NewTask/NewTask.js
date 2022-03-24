e27842af1d389a48428c13e6c03fca354b2f0251
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-24 12:48:53 +0100

    TASK_MANAGER_Custom_Hooks: using the hook in newTask - bind to preconfigure functions

--- 10_TASK_MANAGER_Custom_Hooks/src/components/NewTask/NewTask.js
+++ 10_TASK_MANAGER_Custom_Hooks/src/components/NewTask/NewTask.js
@@ -1,41 +1,35 @@
-import { useState } from "react";
+import useHttp from "../hooks/use-http";
 
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  // we need to add taskText
+  // the problem is that the invoking functions has just an argument (textData)
+  // this is an opportunity to preconfigure createTask, by using bind (see line 31)
+  // then the first argument (taskText) will be added y the bind function
+  // and the argument we receives will be appended automatically as the second one
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-7b575-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: "https://react-http-7b575-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
   };
 
   return (

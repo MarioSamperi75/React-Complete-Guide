6f918e06ab43ddf2f3e7781b53acc458e5d990ae
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-24 10:23:05 +0100

    TASK_MANAGER_Custom_Hooks: hook dependencies -  avoid the infinite loop

--- 10_TASK_MANAGER_Custom_Hooks/src/App.js
+++ 10_TASK_MANAGER_Custom_Hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
@@ -7,7 +7,16 @@ import useHttp from "./components/hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (taskObj) => {
+  // to avoid the infinite loop in use-http.js-sendRequest
+  // we have to stop the continous generation of transformTasks(applyData)
+  // by using a useCallback
+  // but to block the first useHttp argument (the object with url)
+  // we have to change the structure in the use-http hook
+  // and move as argument in the sendRequest function
+  // then it's not anymore an external variable in the useCallback, but internal.
+
+  // setTask never change, all the setState can't change! We need no dependencies!
+  const transformTasks = useCallback((taskObj) => {
     const loadedTasks = [];
 
     for (const taskKey in taskObj) {
@@ -15,23 +24,21 @@ function App() {
     }
 
     setTasks(loadedTasks);
-  };
+  }, []);
 
   // we use the hook and we add the specific trasformation function
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: "https://react-http-7b575-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-    },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
 
+  // pay attention to fecthTask as dependency:
+  // fetchTasks, as all the functions, is generated for every re-rendering -> infinite loop!
+  // we must be stop the fetchTasks generation by using an useCallback in use-http.js
+
+  // we pass the url as argument because we moved the request config as argument in sendRequests (use-http.js)
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({
+      url: "https://react-http-7b575-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+    });
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- 10_TASK_MANAGER_Custom_Hooks/src/components/hooks/use-http.js
+++ 10_TASK_MANAGER_Custom_Hooks/src/components/hooks/use-http.js
@@ -1,36 +1,43 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = (applyData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    // we check if there is method, headers, and body
-    // it happens only in the case of post requests
-    try {
-      const response = await fetch(requestConfig.url, {
-        method: requestConfig.method ? requestConfig.method : "GET",
-        headers: requestConfig.headers ? requestConfig.headers : {},
-        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-      });
+  // useCallback to avoid the sendRequest(fetchTasks) regeneration
+  // but requestConfig and applyData are functions
+  // we can move the first one as argument in sendRequest(-> not external anymore!)
+  // and we manage (useCallback) the second one in app.js
+  const sendRequest = useCallback(
+    async (requestConfig) => {
+      setIsLoading(true);
+      setError(null);
+      // we check if there is method, headers, and body
+      // it happens only in the case of post requests
+      try {
+        const response = await fetch(requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : "GET",
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        });
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
+        if (!response.ok) {
+          throw new Error("Request failed!");
+        }
+
+        const data = await response.json();
+
+        // fuction that we get as argument
+        // the trasformation of the data is different in get or post requests
+        // we delegate the trasformation function to the component that uses this hook
+        applyData(data);
+      } catch (err) {
+        setError(err.message || "Something went wrong!");
       }
-
-      const data = await response.json();
-
-      // fuction that we get as argument
-      // the trasformation of the data is different in get or post requests
-      // we delegate the trasformation function to the component that uses this hook
-      applyData(data);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
+      setIsLoading(false);
+    },
+    [applyData]
+  );
   // this syntax is the same as isLoading: isLoading, ... and so on
   return {
     isLoading,

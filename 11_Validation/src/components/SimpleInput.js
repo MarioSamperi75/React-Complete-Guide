1fbe66d5ab51c2ec8e74f647a973d36ee94d8082
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-25 14:57:37 +0100

    Validation: creating the emailInput

--- 11_Validation/src/components/SimpleInput.js
+++ 11_Validation/src/components/SimpleInput.js
@@ -2,29 +2,43 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
   // we manage enterNameIsValide some a normal variable and we derive it from enteredName
   // we don't really need two states
   // so we can delete all the corresponding setState- Cleaner code!
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
+  const enteredEmailIsValid = enteredEmail.includes("@");
   // we will check INvalid to render conditionallu components and style
   const enteredNameIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const enteredEmailIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
   // we can check the validity of the overall form in case there is many inputs fields
   // now we can for example disable the button in !formIsValid
   let formIsValid = false;
 
   // in case we are many inputs we just use && (&& enteredAgeIsValid...)
-  if (enteredNameIsValid) {
+  if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true;
   }
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
+
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
   // we introduce a validation when the input lose the focus (blur)
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
+  };
+
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
   };
 
   const formSubmissionHandler = (event) => {
@@ -38,11 +52,17 @@ const SimpleInput = (props) => {
 
     //resetting logic
     setEnteredName("");
+    setEnteredEmail("");
     setEnteredNameTouched(false);
+    setEnteredEmailTouched(false);
   };
 
   // setting the name of the class dinamically
   const nameInputClass = enteredNameIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClass = enteredEmailIsInvalid
     ? "form-control invalid"
     : "form-control";
 
@@ -59,6 +79,19 @@ const SimpleInput = (props) => {
         />
         {enteredNameIsInvalid && (
           <p className="error-text">No empty input, please!</p>
+        )}
+      </div>
+      <div className={emailInputClass}>
+        <label htmlFor="email">Your E-Mail</label>
+        <input
+          type="text"
+          id="email"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+        />
+        {enteredEmailIsInvalid && (
+          <p className="error-text">Enter a valid Email, plese!</p>
         )}
       </div>
       <div className="form-actions">

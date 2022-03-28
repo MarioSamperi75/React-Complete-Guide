563c409a960c6f35b6b1a6f558b8050c56178969
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-28 15:50:16 +0200

    REACT_MEALS_HTTP: fetching data from DB

--- 12_REACTMEALS_HTTP/src/components/Meals/AvailableMeals.js
+++ 12_REACTMEALS_HTTP/src/components/Meals/AvailableMeals.js
@@ -1,41 +1,43 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-
-// we doesn't get things from db,
-// we hav just some dummy data
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 // we transform the dummy array in a jsx element array
 // we do it in a variable (mealsList)
 const AvailableMeals = (props) => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
+  const [meals, setMeals] = useState([]);
+
+  // we load data  from firebase as side effect
+  // useEffect function can't return a promise, but can contain a function that returns a promise
+  // so we have to create a nested function(fetchMeals) and call it.
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-7b575-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      // we convert to jsobject
+      const responseData = await response.json();
+
+      // we convert the object to an array
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      // we save the array as state
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+  //we go through the new meals array (the state)
+  const mealsList = meals.map((meal) => {
     return (
       <MealItem
         id={meal.id} // this is new!

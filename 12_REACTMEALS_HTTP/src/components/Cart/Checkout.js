edce66b555b8eb44f0756164e651415ea8e8256b
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-29 10:03:40 +0200

    REACT_MEALS_HTTP: creating cancel button the Checkout form

--- 12_REACTMEALS_HTTP/src/components/Cart/Checkout.js
+++ 12_REACTMEALS_HTTP/src/components/Cart/Checkout.js
@@ -1,5 +1,8 @@
 import classes from "./Checkout.module.css";
 
+// we add a new cancel button, type 'button' to not submit
+// we receive a props on cancle from Card
+// in Card this action is associated with closing the form
 const Checkout = (props) => {
   return (
     <form>
@@ -19,6 +22,9 @@ const Checkout = (props) => {
         <label htmlFor="city">City</label>
         <input type="text" id="city" />
       </div>
+      <button type="button" onClick={props.onCancel}>
+        Cancel
+      </button>
       <button>Confirm</button>
     </form>
   );

483fdc0767da1fe7f0f36b30d0146afddbf78d5c
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-30 14:53:11 +0200

    RESUX_COUNTER: providing the store

--- 14_REDUX_COUNTER/src/index.js
+++ 14_REDUX_COUNTER/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// PROVIDING THE STORE -REDUX-
+//1) import the provider
+//2) wrap what you need -maybe the entire app- with the provider
+//3) import the store
+//4) add the store to the provider as props
 
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+import "./index.css";
+import App from "./App";
+import store from "./store/index";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);

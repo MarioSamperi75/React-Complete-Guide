c361b9c2f27d463e6b8c427122c4cef84eac0a1e
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-03-31 10:22:15 +0200

    REDUX_COUNTER: Implement the toggle button, working with multiple state

--- 14_REDUX_COUNTER/src/store/index.js
+++ 14_REDUX_COUNTER/src/store/index.js
@@ -1,9 +1,18 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+// of course you should consider these states as local
+// and use useState in the component instead of Redux
+// But this is just a redux demo
+const initialState = { counter: 0, showCounter: true };
+
+// when working with moltiple properties
+// you have always return the entire payload
+// even if the other state don't change
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
@@ -11,12 +20,21 @@ const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
 

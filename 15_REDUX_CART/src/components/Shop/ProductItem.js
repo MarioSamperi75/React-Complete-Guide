fb8a63254ff1e3a35b3829f773db43a301033194
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-01 16:19:43 +0200

    REDUX_CART:  asynchronous requests, the component solution

--- 15_REDUX_CART/src/components/Shop/ProductItem.js
+++ 15_REDUX_CART/src/components/Shop/ProductItem.js
@@ -1,22 +1,62 @@
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../store/cart-slice";
 
-const ProductItem = (props) => {
-  const { title, price, description, id } = props;
+// we transfered the logic into the component to send the modified object to the db
+// the asynchronous code can't be used in the reducer
+// but the trasformation itself isn't asyncronous
+// so thas should be used in the reducers
+// it's just the request that is asynchronous!
+// this code is sub-optimal
 
+const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
+  const { title, price, description, id } = props;
+
   const addToCartHandler = () => {
-    dispatch(
-      cartActions.addItemToCart({
-        id,
-        title,
-        price,
-      })
-    );
+    const newTotalQuantity = cart.totalQuantity + 1;
+
+    const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
+    const existingItem = updatedItems.find((item) => item.id === id);
+    if (existingItem) {
+      const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+      updatedItem.quantity++;
+      updatedItem.totalPrice = updatedItem.totalPrice + price;
+      const existingItemIndex = updatedItems.findIndex(
+        (item) => item.id === id
+      );
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({
+        id: id,
+        price: price,
+        quantity: 1,
+        totalPrice: price,
+        name: title,
+      });
+    }
+
+    const newCart = {
+      totalQuantity: newTotalQuantity,
+      items: updatedItems,
+    };
+
+    dispatch(cartActions.replaceCart(newCart));
+
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    // dispatch(
+    //   cartActions.addItemToCart({
+    //     id,
+    //     title,
+    //     price,
+    //   })
+    // );
   };
 
   return (

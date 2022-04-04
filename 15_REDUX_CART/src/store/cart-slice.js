ad32037d1bca620ac254010ddcdbf6b18a9e97ac
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-04 16:26:40 +0200

    moving the action creator in the cart-slice

--- 15_REDUX_CART/src/store/cart-slice.js
+++ 15_REDUX_CART/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -44,6 +45,51 @@ const cartSlice = createSlice({
     },
   },
 });
+
+//  ACTION CREATOR: a function returning a function
+//  we can dispatch action in it AND execute requests
+//  it is not possible to use asunc code in the reducer
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data...",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-7b575-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Something cart data failed");
+      }
+    };
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Fail to send data",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
--- 15_REDUX_CART/src/store/ui-slice.js
+++ 15_REDUX_CART/src/store/ui-slice.js
@@ -20,50 +20,5 @@ const uiSlice = createSlice({
   },
 });
 
-//  ACTION CREATOR: a function returning a function
-//  we can dispatch action in it AND execute requests
-//  it is not possible to use asunc code in the reducer
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data...",
-      })
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(
-        "https://react-http-7b575-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Something cart data failed");
-      }
-    };
-    try {
-      await sendRequest();
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error",
-          message: "Fail to send data",
-        })
-      );
-    }
-  };
-};
-
 export const uiActions = uiSlice.actions;
 export default uiSlice;

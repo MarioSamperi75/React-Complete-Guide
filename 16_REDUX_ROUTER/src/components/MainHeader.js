519d831677e0b8dc752eaa6eb7507841002a3a1b
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-05 21:33:04 +0200

    REACT_ROUTER: adding css

--- 16_REDUX_ROUTER/src/components/MainHeader.js
+++ 16_REDUX_ROUTER/src/components/MainHeader.js
@@ -1,23 +1,22 @@
 import { Link } from "react-router-dom";
 
+import classes from "./MainHeader.module.css";
+
 // The Link component allows to prevent the reloading of the page
 // reloading should mean losing all the states...
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
             <Link to="/welcome">Welcome</Link>
           </li>
-        </ul>
-        <ul>
           <li>
             <Link to="/products">Products</Link>
           </li>
         </ul>
-        Li
       </nav>
     </header>
   );

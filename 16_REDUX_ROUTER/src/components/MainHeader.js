cce63b9df1a579dc7ec17fdbe206f878e3280d5e
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-05 21:16:26 +0200

    REACT_ROUTER: the traditional link

---
+++ 16_REDUX_ROUTER/src/components/MainHeader.js
@@ -0,0 +1,20 @@
+const MainHeader = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/welcome">Welcome</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="/products">Products</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;

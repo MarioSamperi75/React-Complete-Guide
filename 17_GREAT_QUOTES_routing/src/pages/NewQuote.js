9c8cd555b26bfeca822471c4cb1177ac2a24eefd
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-06 15:54:52 +0200

    GREAT_QUOTES: programmatic navigation to redirect after submitting a new quote

--- 17_GREAT_QUOTES_routing/src/pages/NewQuote.js
+++ 17_GREAT_QUOTES_routing/src/pages/NewQuote.js
@@ -1,8 +1,17 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
+
+// useHistory allows the programmatic navigation
+//  push allows create a new history entry
+//  replace allows to replace the current entry without creting a new one
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push("/quotes");
   };
 
   return (

6a5905a775043f5ae64a9b521b2e9c8c7a8f6621
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-06 15:24:46 +0200

    GREAT_QUOTES: outputting data on the detail page

--- 17_GREAT_QUOTES_routing/src/pages/QuoteDetail.js
+++ 17_GREAT_QUOTES_routing/src/pages/QuoteDetail.js
@@ -1,12 +1,36 @@
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Albert Einstein",
+    text: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+  },
+  {
+    id: "q2",
+    author: "Mother Teresa",
+    text: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+  },
+  {
+    id: "q3",
+    author: "Walt Disney",
+    text: "The way to get started is to quit talking and begin doing.",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <h1>No quote found</h1>;
+  }
   return (
     <section>
-      <h1>QUOTE DETAIL</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote author={quote.author} text={quote.text} />
       <Route path="/quotes/:quotesId/comments">
         <Comments />
       </Route>

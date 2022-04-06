8380319b5100a47d3fcf5fb762958fa7fca5b70a
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-06 14:26:57 +0200

    GREAT_QUOTES: passing items to a QuoteList component

--- 17_GREAT_QUOTES_routing/src/pages/Quotes.js
+++ 17_GREAT_QUOTES_routing/src/pages/Quotes.js
@@ -1,9 +1,25 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Albert Einstein",
+    text: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+  },
+  {
+    id: "q2",
+    author: "Mother Teresa",
+    text: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+  },
+  {
+    id: "q3",
+    author: "Walt Disney",
+    text: "The way to get started is to quit talking and begin doing.",
+  },
+];
+
 const Quotes = () => {
-  return (
-    <section>
-      <h1>QUOTES</h1>
-    </section>
-  );
+  return <QuoteList quotes={DUMMY_QUOTES}></QuoteList>;
 };
 
 export default Quotes;

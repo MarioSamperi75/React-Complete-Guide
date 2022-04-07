0e3888ce0703e3a14c92dfe63a6ae668d0663834
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-07 11:31:52 +0200

    GREAT_QUOTES: ordering the list by working with query parameters

--- 17_GREAT_QUOTES_routing/src/components/quotes/QuoteList.js
+++ 17_GREAT_QUOTES_routing/src/components/quotes/QuoteList.js
@@ -1,13 +1,51 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+// #1 check the url (isSortingAscending)
+// #2 create a button  and a method to change the URL and switch parameter
+// #3 Adapt the text in the button dynamically
+// #4 Create the sorting function, pass the quotes and the order
+//    and get your sorted quotes to use in the rendering
+
+// helper function that sorts quotes by id
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  // this object transform the search location string to a js object
+  // that is easier to use (?sort=asc  => sort : 'asc')
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  //just updating the URL
+  const changeSortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Ascending" : "Descending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}

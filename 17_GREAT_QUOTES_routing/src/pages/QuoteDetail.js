acd3f1d80a319c26b47618d70e5df4dc3ab1afe0
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-07 21:39:30 +0200

    GREAT_QUOTES: loading quotes in the detail page

--- 17_GREAT_QUOTES_routing/src/pages/QuoteDetail.js
+++ 17_GREAT_QUOTES_routing/src/pages/QuoteDetail.js
@@ -1,24 +1,29 @@
+import { useEffect } from "react";
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
-const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Albert Einstein",
-    text: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
-  },
-  {
-    id: "q2",
-    author: "Mother Teresa",
-    text: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
-  },
-  {
-    id: "q3",
-    author: "Walt Disney",
-    text: "The way to get started is to quit talking and begin doing.",
-  },
-];
+//we use hooks and APIs exact as we did in quote.js
+// const DUMMY_QUOTES = [
+//   {
+//     id: "q1",
+//     author: "Albert Einstein",
+//     text: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+//   },
+//   {
+//     id: "q2",
+//     author: "Mother Teresa",
+//     text: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+//   },
+//   {
+//     id: "q3",
+//     author: "Walt Disney",
+//     text: "The way to get started is to quit talking and begin doing.",
+//   },
+// ];
 
 // we use an UseRouteMatch to get info about the actual path
 // and write the address dynamically
@@ -29,9 +34,33 @@ const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
 
-  if (!quote) {
+  const { quoteId } = params;
+
+  // in this case send request needs a parameter
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <h1>No quote found</h1>;
   }
 
@@ -43,7 +72,7 @@ const QuoteDetail = () => {
 
   return (
     <section>
-      <HighlightedQuote author={quote.author} text={quote.text} />
+      <HighlightedQuote author={loadedQuote.author} text={loadedQuote.text} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>

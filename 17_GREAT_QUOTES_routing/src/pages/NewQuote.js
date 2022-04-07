5ef498aa13ba2b0182f61f2a310dfd22ac66d75f
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-07 14:45:51 +0200

    GREAT_QUOTES: sending new  quotes to firebase

--- 17_GREAT_QUOTES_routing/src/pages/NewQuote.js
+++ 17_GREAT_QUOTES_routing/src/pages/NewQuote.js
@@ -1,22 +1,41 @@
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
+import { useEffect } from "react";
 
 // useHistory allows the programmatic navigation
 //  push allows create a new history entry
 //  replace allows to replace the current entry without creting a new one
 
 const NewQuote = () => {
+  // initilizing the hook: we pass the method that we imported
+  // and we get the sendMEthod and the status
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
+  // we get the sendRequest method from the hook
+  // (we passed the addQuote method when we initialized the hook)
+  // and we pass the data
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    sendRequest(quoteData);
 
     history.push("/quotes");
   };
 
   return (
+    // we send isLoading to conditionally render the 'waiting' spinner
     <section>
-      <QuoteForm onAddQuote={addQuoteHandler} />
+      <QuoteForm
+        isLoading={status === "pending"}
+        onAddQuote={addQuoteHandler}
+      />
     </section>
   );
 };

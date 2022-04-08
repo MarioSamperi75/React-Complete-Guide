bc3c7763163dce646a23841703d6b532870deb27
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-08 12:57:24 +0200

    GREAT_QUOTES: adding Comments

--- 17_GREAT_QUOTES_routing/src/components/comments/NewCommentForm.js
+++ 17_GREAT_QUOTES_routing/src/components/comments/NewCommentForm.js
@@ -1,26 +1,56 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
-import classes from './NewCommentForm.module.css';
+import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
 
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  // when the status become completed
+  // (that means that a new comment is added),
+  // use Effect notice it and
+  // run a function in the parent component (to fecth again the comments)
+  // we get this function as props
+
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
+
+  // we get sendRequest from the hook
+  // (addComment as API)
+  // and we call it with the entered text and the quoteId
+  // that we receive as props
   const submitFormHandler = (event) => {
     event.preventDefault();
 
+    const enteredText = commentTextRef.current.value;
+
     // optional: Could validate here
 
-    // send comment to server
+    sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );

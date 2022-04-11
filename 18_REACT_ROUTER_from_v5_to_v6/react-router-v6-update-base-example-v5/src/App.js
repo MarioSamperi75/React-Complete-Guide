9821e0514f1bb61eb77994e60aab22a981ae55ab
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-11 15:15:43 +0200

    React_Router_v6: removing exact - new internal path logic

--- 18_REACT_ROUTER_from_v5_to_v6/react-router-v6-update-base-example-v5/src/App.js
+++ 18_REACT_ROUTER_from_v5_to_v6/react-router-v6-update-base-example-v5/src/App.js
@@ -1,6 +1,13 @@
 // #1 Switch becomes Routes
 // #2  Write the child-components as attribut (element={})
 
+// #3 we can delete exact. the exact matching becomes the standard behavior
+//    if you desire the old behavior you have to write the path like this:
+//    path="/products/*"
+//    that will match all the path starting with products
+//    the new react router search anyway the best matching
+//    even if we write it later: the order of the paths is no longer important
+
 import { Route, Routes } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
@@ -15,7 +22,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/welcome" element={<Welcome />} />
-          <Route path="/products" exact element={<Products />} />
+          <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<ProductDetail />} />
         </Routes>
       </main>

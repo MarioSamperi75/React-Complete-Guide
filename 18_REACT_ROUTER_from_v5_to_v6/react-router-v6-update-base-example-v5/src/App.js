166e72b7ff2d5c7a21630e7bf7dccacf2d4cdaf3
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-11 15:01:10 +0200

    React_Router_v6: 'Routes' and 'element'

--- 18_REACT_ROUTER_from_v5_to_v6/react-router-v6-update-base-example-v5/src/App.js
+++ 18_REACT_ROUTER_from_v5_to_v6/react-router-v6-update-base-example-v5/src/App.js
@@ -1,26 +1,23 @@
-import { Route, Switch } from 'react-router-dom';
+// #1 Switch becomes Routes
+// #2  Write the child-components as attribut (element={})
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+import { Route, Routes } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Switch>
-          <Route path='/welcome'>
-            <Welcome />
-          </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
       </main>
     </div>
   );

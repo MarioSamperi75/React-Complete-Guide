641de1b6f1f825f5123942efe41ef5c05b06fdcd
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-22 10:53:51 +0200

    React_Router_v6: exact path as default: relativ links

--- 18_REACT_ROUTER_from_v5_to_v6/react-router-v6-update-second-example-v5/src/pages/Welcome.js
+++ 18_REACT_ROUTER_from_v5_to_v6/react-router-v6-update-second-example-v5/src/pages/Welcome.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 // React router v6 assumes that you are starting with the parent route
 // no need to write it, just the nested route!
@@ -6,8 +6,9 @@ const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
+      <Link to="new-user">New User</Link>
       <Routes>
-        <Route path="/new-user" element={<p>Welcome, new user!</p>} />
+        <Route path="new-user" element={<p>Welcome, new user!</p>} />
       </Routes>
     </section>
   );

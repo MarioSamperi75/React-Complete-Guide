1cfa0e79de4009de68b516934053c55c1fe028ed
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-25 13:31:37 +0200

    REACT_AUTH: change password - sending the token to a protected API

--- 20_REACT_AUTH/src/components/Profile/ProfileForm.js
+++ 20_REACT_AUTH/src/components/Profile/ProfileForm.js
@@ -1,17 +1,55 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
+import AuthContext from "../../store/auth-context";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyA3zOxsuKzbaaDE0gwN_CRlSBAQgYy_BFs",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      // you can handle the error here
+      // in this case to keep it easy
+      // we add the attribute minLength in the component
+      // (it's easy to manipulate, we should handle the error...)
+      .then((res) => {
+        //assumption: always succeed!
+      });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          ref={newPasswordInputRef}
+          minLength="6"
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;

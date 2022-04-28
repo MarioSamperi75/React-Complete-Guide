27223040d59a086f3a900179fd8b02a7c59df1bd
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-04-28 09:38:44 +0200

    Next.js: basic routing

---
+++ 21_nextjs_course/pages/index.js
@@ -0,0 +1,8 @@
+// index.js is the root : our-domain/
+
+//Start the project with npm run dev
+const HomePage = () => {
+  return <h1>The Home Page</h1>;
+};
+
+export default HomePage;
---
+++ 21_nextjs_course/pages/news.js
@@ -0,0 +1,9 @@
+// all the other files in the map,
+// create a new path from the root
+// in this case our-domain/news
+
+const NewsPage = () => {
+  return <h1>The News Page</h1>;
+};
+
+export default NewsPage;

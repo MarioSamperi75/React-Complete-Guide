630df180ea877f76a0250864b21f198a6491b017
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2022-12-12 14:17:10 +0100

    Adding Custom Components & CSS Modules

--- 21_nextjs_course/2_React_Meetups/pages/[meetupsId]/index.js
+++ 21_nextjs_course/2_React_Meetups/pages/[meetupsId]/index.js
@@ -1,8 +1,17 @@
-const NewPage = () => {
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-return (
-    NewPage
-)
-}
+const MeetupDetails = () => {
+  return (
+  
+       <MeetupDetail
+        image="https://www.celebritycruises.com/blog/content/uploads/2022/06/messina-italy-history-culture-view-hero-1600x890.jpg"
+        title="Messina"
+        address="Contrada Citola, 21"
+        description="Cooperativa prosperità"
+      /> 
 
-export default NewPage;
+  );
+};
+
+export default MeetupDetails;

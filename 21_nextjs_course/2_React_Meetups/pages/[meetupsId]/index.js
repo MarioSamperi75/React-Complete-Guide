0806a5bc7949bbb7ba669a45752f16d5649fd073
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2022-12-13 14:34:43 +0100

    dynamic pages, getStaticPaths, fallback

--- 21_nextjs_course/2_React_Meetups/pages/[meetupsId]/index.js
+++ 21_nextjs_course/2_React_Meetups/pages/[meetupsId]/index.js
@@ -3,15 +3,50 @@ import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetails = () => {
   return (
-  
-       <MeetupDetail
-        image="https://www.celebritycruises.com/blog/content/uploads/2022/06/messina-italy-history-culture-view-hero-1600x890.jpg"
-        title="Messina"
-        address="Contrada Citola, 21"
-        description="Cooperativa prosperità"
-      /> 
-
+    <MeetupDetail
+      image="https://www.celebritycruises.com/blog/content/uploads/2022/06/messina-italy-history-culture-view-hero-1600x890.jpg"
+      title="Messina"
+      address="Contrada Citola, 21"
+      description="Cooperativa prosperità"
+    />
   );
+};
+export const getStaticPaths = async (context) => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupsId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupsId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupsId = context.params.meetupsId;
+
+  // fetch data from api
+  return {
+    //fetch data for a sngle meetup
+    props: {
+      meetupData: {
+        image:
+          "https://www.celebritycruises.com/blog/content/uploads/2022/06/messina-italy-history-culture-view-hero-1600x890.jpg",
+        id: meetupsId,
+        title: "Messina",
+        address: "Contrada Citola, 21",
+        description: "Cooperativa prosperità",
+      },
+    },
+    revalidate: 10,
+  };
 };
 
 export default MeetupDetails;

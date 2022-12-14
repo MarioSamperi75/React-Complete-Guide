1ca8ac9719c1485aaa317c1c152e59ebcf79199a
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2022-12-14 09:38:59 +0100

    Sending Http Requests To Our API Routes

--- 21_nextjs_course/2_React_Meetups/pages/api/new-meetup.js
+++ 21_nextjs_course/2_React_Meetups/pages/api/new-meetup.js
@@ -7,12 +7,18 @@ const handler = async (req, res) => {
 
     if (req.method === 'POST') {
         const data = req.body;  
-
+            
         const client= await MongoClient.connect('mongodb+srv://mariosss:Ssb2017!@cluster0.7wrvi.mongodb.net/meetups?retryWrites=true&w=majority');
         const db = client.db();
 
         const meetupsCollection = db.collection('meetups');
-        meetupsCollection.insertOne(data);
+        const result = await meetupsCollection.insertOne(data);
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({message: 'Meetup inserted!'});
     }
 }
 

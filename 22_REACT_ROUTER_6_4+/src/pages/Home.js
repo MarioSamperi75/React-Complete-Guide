08cd4d7491ff197d86eb2911870f6b8e4334fc69
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-06-13 18:49:52 +0200

    linking with Link element -  good!

--- 22_REACT_ROUTER_6_4+/src/pages/Home.js
+++ 22_REACT_ROUTER_6_4+/src/pages/Home.js
@@ -1,10 +1,17 @@
-//the a element will refresh the page. No good!
+// the a element will refresh the page. No good!
+
+import { Link } from "react-router-dom";
+
+// the link element will not refresh the page
 const Homepage = () => {
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         <a href="/products">Go To Product Old Style</a>
+      </p>
+      <p>
+        <Link to="/products">Go To Product with Link</Link>
       </p>
     </>
   );

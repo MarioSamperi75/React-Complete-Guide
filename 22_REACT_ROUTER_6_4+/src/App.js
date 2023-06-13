099bd5af4a891510644451fc379f86c62a71a2d8
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-06-13 22:31:00 +0200

    Handling the Error Page

--- 22_REACT_ROUTER_6_4+/src/App.js
+++ 22_REACT_ROUTER_6_4+/src/App.js
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 // You can have many Layouts with children and so on...
 
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <Homepage /> },
       { path: "/products", element: <ProductsPage /> },

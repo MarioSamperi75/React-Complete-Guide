fceab005114be5d5e6740c50fd9586a6684a0114
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-06-13 18:40:29 +0200

    linking with a link - no good!

--- 22_REACT_ROUTER_6_4+/src/pages/Home.js
+++ 22_REACT_ROUTER_6_4+/src/pages/Home.js
@@ -1,5 +1,13 @@
+//the a element will refresh the page. No good!
 const Homepage = () => {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        <a href="/products">Go To Product Old Style</a>
+      </p>
+    </>
+  );
 };
 
 export default Homepage;

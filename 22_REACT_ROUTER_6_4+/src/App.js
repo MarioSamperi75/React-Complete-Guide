e4712b76809aed53ff9b9eb2ddc620b9970852c4
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-06-13 16:11:57 +0200

    adding a second route

--- 22_REACT_ROUTER_6_4+/src/App.js
+++ 22_REACT_ROUTER_6_4+/src/App.js
@@ -1,7 +1,26 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  //createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Homepage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
-const router = createBrowserRouter([{ path: "/", element: <Homepage /> }]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Homepage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
+/* const router = createBrowserRouter([
+  { path: "/", element: <Homepage /> },
+  { path: "/products", element: <ProductsPage /> },
+]); */
 
 function App() {
   return <RouterProvider router={router} />;

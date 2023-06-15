e3acd3fd3b8a1428ba2ce5d37a8aedbc3c1251a3
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-06-15 15:46:12 +0200

    Using Redux router loader

--- 24_React_Router6_4+_advanced_features/frontend/src/App.js
+++ 24_React_Router6_4+_advanced_features/frontend/src/App.js
@@ -1,27 +1,41 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from './pages/EditEvent';
-import EventDetailPage from './pages/EventDetail';
-import EventsPage from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
-import RootLayout from './pages/Root';
+import EditEventPage from "./pages/EditEvent";
+import EventDetailPage from "./pages/EventDetail";
+import EventsPage from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
-      { index: true, element: <HomePage /> },
       {
-        path: 'events',
+        index: true,
+        element: <EventsPage />,
+        loader: async () => {
+          const response = await fetch("http://localhost:8080/events");
+
+          if (!response.ok) {
+            // ...
+          } else {
+            const resData = await response.json();
+            return resData.events; // the backend har an events object
+            //react router will make the return value available in the element (EventsPage)
+          }
+        },
+      },
+      {
+        path: "events",
         element: <EventsRootLayout />,
         children: [
           { index: true, element: <EventsPage /> },
-          { path: ':eventId', element: <EventDetailPage /> },
-          { path: 'new', element: <NewEventPage /> },
-          { path: ':eventId/edit', element: <EditEventPage /> },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
         ],
       },
     ],

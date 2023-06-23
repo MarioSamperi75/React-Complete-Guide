39e18067b23cc1d3af42f42c7e411832520d33b9
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-06-23 23:18:55 +0200

    fixing newItems actions bug - protecting events/new and /edit routes

--- 25_Authentication/frontend/src/App.js
+++ 25_Authentication/frontend/src/App.js
@@ -17,7 +17,7 @@ import AuthenticationPage, {
   action as authAction,
 } from "./pages/Authentication";
 import { action as logoutAction } from "./pages/Logout";
-import { getAuthToken as tokenLoader } from "./util/auth";
+import { checkAuthLoader, getAuthToken as tokenLoader } from "./util/auth";
 
 const router = createBrowserRouter([
   {
@@ -55,6 +55,7 @@ const router = createBrowserRouter([
               {
                 path: "edit",
                 element: <EditEventPage />,
+                loader: checkAuthLoader,
                 action: manipulateEventAction,
               },
             ],
@@ -62,6 +63,7 @@ const router = createBrowserRouter([
           {
             path: "new",
             element: <NewEventPage />,
+            loader: checkAuthLoader,
             action: manipulateEventAction,
           },
         ],
